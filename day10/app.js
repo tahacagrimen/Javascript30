5cc6f4cddab7b0c23f6097641a04c47b62eb1599
tahacagrimen <tahacagrimen2@gmail.com>
2022-06-16 17:55:44 +0300

    Hold Shift to Check Multiple Checkboxes

--- day10/app.js
+++ day10/app.js
@@ -3,8 +3,18 @@ const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 let lastChecked;
 
 function handleCheck(e) {
-  if (e.shifKey && this.checked) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
   }
+  lastChecked = this;
 }
 
 checkboxes.forEach((checkbox) => {
